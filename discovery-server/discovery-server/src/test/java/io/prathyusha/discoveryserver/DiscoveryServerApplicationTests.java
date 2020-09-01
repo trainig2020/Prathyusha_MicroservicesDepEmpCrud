42c6af555ef96161afbb1786ce0c19df32a2e865
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-02 00:01:54 +0530

    Add files via upload

---
+++ discovery-server/discovery-server/src/test/java/io/prathyusha/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prathyusha.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
